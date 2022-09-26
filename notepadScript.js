34c9e2edff541e7bec05573540428890050b6559
lucazed19 <lucazed19@outlook.com.br>
2022-09-26 15:58:24 -0300

    Project Omega 0.5.4

--- notepadScript.js
+++ notepadScript.js
@@ -32,13 +32,37 @@ function dragNote(elmnt) {
     }
 }
 dragNote(document.getElementById("notepad"))
+dragNote(document.getElementById("notepad2"))
 
 function openNotepad(){
     document.getElementById("notepad").style.display ="block"
 }
 
+let op = 0;
+function openNotepad2(){
+    if(document.getElementById("cmd2").style.display == "block" && op == 0){
+        document.getElementById("notepad2").style.display ="block"
+        document.getElementById("cmd2").style.display = "none";
+        let main = document.getElementById("main2")
+        let limparParagrafos = main.querySelectorAll("p")
+        for (let i = 0; limparParagrafos[i]; i++) {
+        limparParagrafos[i].innerText = "";
+        limparParagrafos[i].parentNode.removeChild(limparParagrafos[i])
+        }
+        for (let i = 0; paragrafos[i]; i++){
+            paragrafos2.pop()
+        }
+        op++
+    }else if(op>0){
+        document.getElementById("notepad2").style.display ="block"
+    }
+}
+
 function minNotepad(){
     document.getElementById("notepad").style.display ="none"
+}
+function minNotepad2(){
+    document.getElementById("notepad2").style.display ="none"
 }
 
 let maxiNotepad = 0
@@ -64,12 +88,41 @@ function maxNotepad(){
         maxiNotepad = 0;
     }
 }
+let maxiNotepad2 = 0
+function maxNotepad2(){
+    let notepad = document.getElementById("notepad2")
+    if (maxiNotepad2 == 0){
+        notepad.style.width = "100%";
+        notepad.style.height = "100%";
+        notepad.style.top = 0
+        notepad.style.left = 0
+        notepad.style.right = 0
+        notepad.style.borderRadius = 0
+        document.getElementById("notepad2Header").style.borderRadius = 0
+        maxiNotepad2 = 1;
+    }else if (maxiNotepad2 == 1){
+        notepad.style.width = "40%";
+        notepad.style.height = "70%";
+        notepad.style.top = "25%"
+        notepad.style.left = "unset";
+        notepad.style.right = "300px"
+        notepad.style.borderRadius = "10px";
+        document.getElementById("notepad2Header").style.borderRadius = "10px 10px 0 0";
+        maxiNotepad2 = 0;
+    }
+}
 
 function closeNotepad(){
     let notepad = document.getElementById("notepad")
     notepad.style.display = "none";
     maxiNotepad = 1
     maxNotepad()
+}
+function closeNotepad2(){
+    let notepad = document.getElementById("notepad2")
+    notepad.style.display = "none";
+    maxiNotepad2 = 1
+    maxNotepad2()
 }
 
 function hover(){
